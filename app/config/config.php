f22d44562e4de0ab9bdefcf68181fc204d8af670
TanPhanCy <phan.tan.brussels@gmail.com>
2017-10-27 15:23:57 +0200

    add

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = array(
+
+	"database" => array(
+		"dbhost" => "localhost",
+		"dbname" => "elders",
+		"dbuser" => "root",
+		"dbpass" => "root"
+	)
+
+);
---
+++ app/views/parts/single.php
@@ -0,0 +1,61 @@
+<?php  /* 
+include '../database/connect.php'
+
+
+  $query = $bdd->query('SELECT * FROM film WHERE `film_titre`="'. $_GET['titre'] .'"' ); */
+
+
+
+
+?>
+//
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<meta content="musique" name="keywords">
+<meta content="Download movies" name="description">
+<link rel="stylesheet" type="text/css" href="../../../public/asset/css/style.css">
+<title>Elders</title>
+</head>
+
+<body>
+<main class="main-content">
+  <div class="container">
+    <div class="page">
+      <div class="breadcrumbs"> <a href="../../../index.php">Back</a> </div>
+      <div class="content">
+        <div class="main">
+          <div class="midle">
+            <figure class="movie-poster"><img src="../../../public/asset/image/movie1.jpg" alt="#"></figure>
+          </div>
+          <div class="midle">
+            <h2 class="movie-title">Titanic</h2>
+            <div class="movie-summary">
+              <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+              <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit sed.</p>
+            </div>
+            <ul class="movie-meta">
+              <li><strong>Rating:</strong>
+                <div class="star-rating" title="Rated 4.00 out of 5"><span style="width:80%"><strong class="rating">4.00</strong> out of 5</span></div>
+              </li>
+              <li><strong>Length:</strong> 98 min</li>
+              <li><strong>Premiere:</strong> 22 March 2013 (USA)</li>
+              <li><strong>Category:</strong> Animation/Adventure/Comedy</li>
+            </ul>
+            <ul class="starring">
+              <li><strong>Directors:</strong> Kirk de Mico (as Kirk DeMico). Chris Sanders</li>
+              <li><strong>Writers:</strong> Chris Sanders (screenplay), Kirk De Micco (screenplay)</li>
+              <li><strong>Stars:</strong> Nicolas Cage, Ryan Reynolds, Emma Stone</li>
+            </ul>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+</main>
+</body>
+</html>
